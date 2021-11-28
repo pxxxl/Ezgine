5988bb8e792925e045a60e3a8907123dadc1d1dd
pxxxl <93241841+pxxxl@users.noreply.github.com>
2021-11-28 20:16:25 +0800

    Uncompleted(4)

--- ezgine.h
+++ ezgine.h
@@ -5,18 +5,13 @@
 
 using namespace std;
 
-static const MassPoint CMASSPOINT = MassPoint(0, 0, 0);
-static const CollisionBox CCOLLISIONBOX = CollisionBox();
-static const ForceField CFORCEFIELD = ForceField();
-
 class Ezgine{
 public:
     Ezgine():objectpool(ObjectPool()),collapsecontroler(CollapseControler())
     ,movecontroler(MoveControler()){}
-    void PutObject(MassPoint&);
+    int PutObjectGetId(Entity&);
     void Step();
     void DeleteObjectById(int);
-    
 protected:
 private:
     Ezgine(const Ezgine&):objectpool(ObjectPool()),collapsecontroler(CollapseControler())
@@ -29,24 +24,22 @@ private:
 class ObjectPool{
 public:
     ObjectPool(){}
-    void AddObject(MassPoint&);
-    void AddField(ForceField&);
-    list<MassPoint> GetObjects(){return this->pointpool;}
-    MassPoint& GetObjectsById(int);
-    list<ForceField> GetFields(){return this->fieldpool;}
-    ForceField& GetFieldsById(int);
-    void DeleteObjectById(int);
-    void DeleteFieldByid(int); 
+    int AddObjectGetId(Entity&);
+    list<Entity> GetObjects(){return this->objectpool;}
+    Entity& GetObjectsById(int);
+    list<ForceField> GetFields();
+    void DeleteObjectById(int); 
 protected:
-    list<MassPoint> pointpool;
-    list<ForceField> fieldpool;
+    list<Entity> objectpool;
 private:
 };
 
 class CollapseControler{
 public:
     CollapseControler(){}
-    
+    list<pair<Entity, Entity> > RoughCollapseJudge(ObjectPool&);
+    list<pair<Entity, Entity> > ExactCollaspeJudge(list<pair<Entity, Entity> >);
+    void Collapse(list<pair<Entity, Entity> >);
 protected:
 private:
 };
@@ -60,23 +53,62 @@ private:
 };
 
 class ForceFieldControler{
-    public:
-    protected:
-    private:
+    friend Ezgine;
+public:
+protected:
+
+private:
 };
 
 //below entities
-class MassPoint{
+class Entity{
 public:
-    MassPoint(int m, int x, int y):mass(m),coordinate_x(x),coordinate_y(y),
-        velocity_x(0), velocity_y(0), tem_force_x(0), tem_force_y(0), id(id_number)
-        ,collisionbox(nullptr){id_number++;}
-    MassPoint(const MassPoint&){}
-    virtual ~MassPoint(){}
-    int GetId(){return id;}
+    Entity(){};
+    virtual ~Entity(){}
+    Entity(const Entity&){};
+    void Move();
+    int GetMass(){return masspoint.GetMass();}
+    double GetX(){return masspoint.GetX();}
+    double GetY(){return masspoint.GetY();}
+    double GetVX(){return masspoint.GetVY();}
+    double GetVY(){return masspoint.GetVX();}
+    double GetFX(){return masspoint.GetFX();}
+    double GetFY(){return masspoint.GetFY();}
+    const list<ForceField>& GetForceFields(){return forcefields;}
+    const CollisionBox& GetCollisionBox(){return collisionbox;}
+    void AddFoeceField(ForceField ff){forcefields.push_back(ff);}
+    void DeleteForceFieldById(int id);
+    void AddFX(double FX){masspoint.AddFX(FX);}
+    void AddFY(double FY){masspoint.AddFY(FY);}
+    void EditVX(double VX){masspoint.EditVX(VX);}
+    void EditVY(double VY){masspoint.EditVY(VY);}
 protected:
-    static int id_number;
-    int id;
+private:
+    MassPoint masspoint;
+    CollisionBox collisionbox;
+    list<ForceField> forcefields;
+};
+class MassPoint{
+    friend Entity;
+public:
+protected:
+    MassPoint(int m, int x, int y):mass(m),coordinate_x(x),coordinate_y(y),
+        velocity_x(0), velocity_y(0), tem_force_x(0), tem_force_y(0){}
+    MassPoint(const MassPoint&){}
+    MassPoint():mass(65535),coordinate_x(0),coordinate_y(0),
+    velocity_x(0), velocity_y(0), tem_force_x(0), tem_force_y(0){}
+    virtual ~MassPoint(){}
+    int GetMass(){return mass;}
+    double GetX(){return coordinate_x;}
+    double GetY(){return coordinate_y;}
+    double GetVX(){return velocity_x;}
+    double GetVY(){return velocity_y;}
+    double GetFX(){return tem_force_x;}
+    double GetFY(){return tem_force_y;}
+    void AddFX(double FX){tem_force_x+=FX;}
+    void AddFY(double FY){tem_force_y+=FY;}
+    void EditVX(double VX){velocity_x = VX;}
+    void EditVY(double VY){velocity_y = VY;}
     int mass;
     double coordinate_x;
     double coordinate_y;
@@ -84,7 +116,6 @@ protected:
     double velocity_y;
     double tem_force_x;
     double tem_force_y;
-    CollisionBox* collisionbox;
 private:
 };
 class CollisionBox{
@@ -93,6 +124,7 @@ public:
     CollisionBox(const CollisionBox&){}
     virtual ~CollisionBox(){};
     virtual void OnCollisionExtraReaction();
+    virtual int GetType(){return 0;}
 protected:
     int type;
     double center_x;
@@ -102,12 +134,17 @@ private:
 };
 class CollisionBox_Round : public CollisionBox{
 public:
+    int GetRadious(){return radious;}
+    virtual int GetType(){return 1;}
 protected:
     int radious;
 private:
 };
 class CollisionBox_Rectangle : public CollisionBox{
 public:
+    int GetWidth(){return width;}
+    int GetHeight(){return height;}
+    virtual int GetType(){return 2;}
 protected:
     int width;
     int height;
@@ -116,10 +153,12 @@ private:
 
 class ForceField{
 public:
-    virtual pair<double, double> force(const MassPoint&){};
+    virtual pair<double, double> force(const Entity&){};
     int GetId(){return id;}
     int GetAttach_id(){return attach_id;}
+    void AttachEntity(int id);
 protected:
+    static int id_num;
     int id;
     int attach_id;
 private:
